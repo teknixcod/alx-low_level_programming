b466aeaa0f4e51f3391934388624c294360d79f1
teknixcod <Agboola_ayokanmi>
2023-08-15 14:52:19 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,42 +5,36 @@
  */
 void times_table(void)
 {
-	/* Define variables */
 	int i, j, k;
 
-	/* Outer loop for rows */
 	for (i = 0; i < 10; i++)
 	{
-		/* Inner loop for columns */
 		for (j = 0; j < 10; j++)
 		{
 			k = j * i;
+
 			if (j == 0)
 			{
 				_putchar(k + '0');
 			}
-			else if (k < 10)
-			{
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(k + '0');
-			}
 			else
-			{
-				_putchar(' ');
-				_putchar((k / 10) + '0');
-				_putchar((k % 10) + '0');
-			}
-
-			if (j < 9)
 			{
 				_putchar(',');
 				_putchar(' ');
-			}
-			else
-			{
-				_putchar('\n');
+
+				if (k < 10)
+				{
+					_putchar(' ');
+				}
+				else
+				{
+					_putchar((k / 10) + '0');
+				}
+
+				_putchar((k % 10) + '0');
 			}
 		}
+
+		_putchar('\n');
 	}
 }
