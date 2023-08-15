ac7e77c3ebae216453aec90bdcadf09e96ce44ee
teknixcod <Agboola_ayokanmi>
2023-08-15 12:22:58 -0700

    4. isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - Checks for character in alphabet
+ * @c: is the character to be checked
+ * Return: 1 for characters in alphabet or 0 for anything else
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 65 && c<= 97) || (c >= 97 && c <= 122 ))
+{
+return (1);
+}
+return (0);
+}
