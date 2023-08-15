e87afe31c8062bf9d43d806431addd2a45d97e58
teknixcod <Agboola_ayokanmi>
2023-08-15 16:47:45 -0700

    13. Nature made the natural numbers; All else is the work of women

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - function that prints the n times table, starting with 0.
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	int i, sum = 0;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 5 == 0) || (i % 3 == 0))
+			sum += i;
+	}
+
+	printf("%d", sum);
+	return (0);
+}
+
