078b3be0708a78a3a488ab23873b01d8fce191b9
teknixcod <Agboola_ayokanmi>
2023-09-01 15:55:26 -0700

    0x0A. C - argc, argv Projects

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
+ * Return: Always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int agmt;
+
+	for (agmt = 0; agmt < argc; agmt++)
+		printf("%s\n", argv[agmt]);
+
+	return (0);
+}
