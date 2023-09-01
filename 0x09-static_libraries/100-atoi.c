57850acac800fc91ea41158e06452f981c8172d8
teknixcod <Agboola_ayokanmi>
2023-09-01 11:59:34 -0700

    0x09. C - Static libraries

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _atoi - convert a string into an integer.
+ *
+ * @s: the string to use.
+ *
+ * Return: integer.
+ */
+
+int _atoi(char *s)
+{
+	int sign = 1, a = 0;
+	unsigned int res = 0;
+
+	while (!(s[a] <= '9' && s[a] >= '0') && s[a] != '\0')
+	{
+		if (s[a] == '-')
+			sign *= -1;
+		a++;
+	}
+	while (s[a] <= '9' && (s[a] >= '0' && s[a] != '\0'))
+	{
+		res = (res * 10) + (s[a] - '0');
+		a++;
+	}
+	res *= sign;
+	return (res);
+}
